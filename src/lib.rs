4242bf282db1776a5d438f94a66eb46ca4ac815b
Nathan Wiebe Neufeldt <wn.nathan@gmail.com>
2020-11-27 15:24:20 -0500

    Add Index trait

--- src/lib.rs
+++ src/lib.rs
@@ -36,12 +36,14 @@ pub fn impl_op(attr: TokenStream, item: TokenStream) -> TokenStream {
             Trait::Unary(name) => impl_unary(function, name),
             Trait::Binary(name) => impl_binary(function, name),
             Trait::Assign(name) => impl_assign(function, name),
+            Trait::Index(name) => impl_index(function, name),
         }
     } else {
         match trait_ {
             Trait::Unary(name) => impl_unary_autoref(function, name),
             Trait::Binary(name) => impl_binary_autoref(function, name),
             Trait::Assign(name) => impl_assign_autoref(function, name),
+            Trait::Index(name) => impl_index_autoref(function, name),
         }
     };
     output.into()
@@ -51,6 +53,7 @@ enum Trait {
     Unary(&'static str),
     Binary(&'static str),
     Assign(&'static str),
+    Index(&'static str),
 }
 
 fn to_trait(function: &str) -> Trait {
@@ -77,6 +80,8 @@ fn to_trait(function: &str) -> Trait {
         "bitxor_assign" => Trait::Assign("BitXorAssign"),
         "shl_assign" => Trait::Assign("ShlAssign"),
         "shr_assign" => Trait::Assign("ShrAssign"),
+        "index" => Trait::Index("Index"),
+        "index_mut" => Trait::Index("IndexMut"),
         _ => panic!("name must identify an operation in core::ops"),
     }
 }
@@ -298,6 +303,121 @@ fn impl_assign_autoref(function: syn::ItemFn, trait_name: &str) -> TokenStream {
     output.into()
 }
 
+fn impl_index(function: syn::ItemFn, trait_name: &str) -> TokenStream {
+    let (lhs_type, rhs_type) = index_types(&function);
+
+    let trait_ident = syn::Ident::new(trait_name, proc_macro2::Span::call_site());
+    let fn_name = &function.sig.ident;
+    let trait_path = quote::quote_spanned!(fn_name.span()=> ::core::ops::#trait_ident);
+
+    let ret = &function.sig.output;
+    let ret_type;
+    let ret = match ret {
+        syn::ReturnType::Type(arrow, typ) => match typ.as_ref() {
+            syn::Type::Reference(typ) => {
+                ret_type = typ.elem.as_ref();
+                syn::ReturnType::Type(*arrow, Box::new(syn::Type::Reference(
+                    syn::TypeReference {
+                        lifetime: None,
+                        .. typ.clone()
+                    }
+                )))
+            }
+            typ => panic!("index operation must return a reference type, found {:?}", typ),
+        }
+        syn::ReturnType::Default => panic!("index operation must return a reference type, found ()"),
+    };
+    let generics = &function.sig.generics;
+
+    let output = if trait_name == "Index" {
+        quote! {
+            impl #generics #trait_path<#rhs_type> for #lhs_type {
+                type Output = #ret_type;
+                fn #fn_name(&self, rhs: #rhs_type) #ret {
+                    #function
+                    #fn_name(self, rhs)
+                }
+            }
+        }
+    } else {
+        quote! {
+            impl #generics #trait_path<#rhs_type> for #lhs_type {
+                fn #fn_name(&mut self, rhs: #rhs_type) #ret {
+                    #function
+                    #fn_name(self, rhs)
+                }
+            }
+        }
+    };
+    output.into()
+}
+
+fn impl_index_autoref(function: syn::ItemFn, trait_name: &str) -> TokenStream {
+    let (lhs_type, rhs_type) = index_types(&function);
+
+    let trait_ident = syn::Ident::new(trait_name, proc_macro2::Span::call_site());
+    let fn_name = &function.sig.ident;
+    let trait_path = quote::quote_spanned!(fn_name.span()=> ::core::ops::#trait_ident);
+
+    let ret = &function.sig.output;
+    let ret_type = match ret {
+        syn::ReturnType::Type(_, typ) => match typ.as_ref() {
+            syn::Type::Reference(typ) => typ,
+            typ => panic!("index operation must return a reference type, found {:?}", typ),
+        }
+        syn::ReturnType::Default => panic!("index operation must return a reference type, found ()"),
+    };
+    let rhs_type_val = remove_reference(rhs_type);
+
+    let ref_ = if trait_name == "Index" {
+        quote! {
+            impl #trait_path<#rhs_type> for #lhs_type {
+                type Output = #ret_type;
+                fn #fn_name(&self, rhs: #rhs_type) #ret {
+                    #function
+                    #fn_name(self, rhs)
+                }
+            }
+        }
+    } else {
+        quote! {
+            impl #trait_path<#rhs_type> for #lhs_type {
+                fn #fn_name(&mut self, rhs: #rhs_type) #ret {
+                    #function
+                    #fn_name(self, rhs)
+                }
+            }
+        }
+    };
+    let val = if let Some(rhs_type) = rhs_type_val {
+        if trait_name == "Index" {
+            quote! {
+                impl #trait_path<#rhs_type> for #lhs_type {
+                    type Output = #ret_type;
+                    fn #fn_name(&self, rhs: #rhs_type) #ret {
+                        self.#fn_name(&rhs)
+                    }
+                }
+            }
+        } else {
+            quote! {
+                impl #trait_path<#rhs_type> for #lhs_type {
+                    fn #fn_name(&mut self, rhs: #rhs_type) #ret {
+                        self.#fn_name(&rhs)
+                    }
+                }
+            }
+        }
+    } else {
+        quote!()
+    };
+    let output = quote! {
+        #ref_
+        #val
+    };
+    output.into()
+}
+
 fn un_type(function: &syn::ItemFn) -> &syn::Type {
     let params = &function.sig.inputs;
     if params.len() != 1 {
@@ -329,6 +449,19 @@ fn assign_types(function: &syn::ItemFn) -> (&syn::Type, &syn::Type) {
     }
     if let (syn::FnArg::Typed(lhs), syn::FnArg::Typed(rhs)) = (&params[0], &params[1]) {
         let lhs = remove_reference(lhs.ty.as_ref()).expect("the first operand of an assignment must be a mutable reference");
+        (lhs, rhs.ty.as_ref())
+    } else {
+        panic!("`self` receivers can only be used in associated methods");
+    }
+}
+
+fn index_types(function: &syn::ItemFn) -> (&syn::Type, &syn::Type) {
+    let params = &function.sig.inputs;
+    if params.len() != 2 {
+        panic!("index operations take exactly two arguments, found {}", params.len());
+    }
+    if let (syn::FnArg::Typed(lhs), syn::FnArg::Typed(rhs)) = (&params[0], &params[1]) {
+        let lhs = remove_reference(lhs.ty.as_ref()).expect("the first operand of `index` must be a reference");
         (lhs, rhs.ty.as_ref())
     } else {
         panic!("`self` receivers can only be used in associated methods");
---
+++ tests/index.rs
@@ -0,0 +1,75 @@
+use impl_ops_proc::impl_op;
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+struct X;
+
+#[impl_op]
+fn index(_: &X, idx: bool) -> &'static str {
+    if idx {
+        "true"
+    } else {
+        "false"
+    }
+}
+
+#[test]
+fn immutable_index() {
+    assert_eq!("true", &X[true]);
+    assert_eq!("false", &X[false]);
+}
+
+enum Category {
+    Subject,
+    Adverb,
+    Verb,
+    Adjective,
+    Object,
+}
+struct Sentence {
+    subject: &'static str,
+    adverb: &'static str,
+    verb: &'static str,
+    adjective: &'static str,
+    object: &'static str,
+}
+impl Sentence {
+    fn new(sub: &'static str, adv: &'static str, verb: &'static str, adj: &'static str, obj: &'static str) -> Self {
+        Self {
+            subject: sub,
+            adverb: adv,
+            verb: verb,
+            adjective: adj,
+            object: obj,
+        }
+    }
+}
+
+#[impl_op]
+fn index(sent: &Sentence, cat: Category) -> &&'static str {
+    match cat {
+        Category::Subject => &sent.subject,
+        Category::Adverb => &sent.adverb,
+        Category::Verb => &sent.verb,
+        Category::Adjective => &sent.adjective,
+        Category::Object => &sent.object,
+    }
+}
+
+#[impl_op]
+fn index_mut<'a>(sent: &'a mut Sentence, cat: Category) -> &'a mut &'static str {
+    match cat {
+        Category::Subject => &mut sent.subject,
+        Category::Adverb => &mut sent.adverb,
+        Category::Verb => &mut sent.verb,
+        Category::Adjective => &mut sent.adjective,
+        Category::Object => &mut sent.object,
+    }
+}
+
+#[test]
+fn index() {
+    let mut sentence = Sentence::new("I", "quickly", "ate", "many", "scones");
+    assert_eq!("quickly", sentence[Category::Adverb]);
+    sentence[Category::Subject] = "You";
+    assert_eq!("You", sentence[Category::Subject]);
+}
